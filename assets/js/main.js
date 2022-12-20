daf214f012265d2cdd8d05874c0fc11f30ffe7e3
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-12-20 19:27:10 -0300

    introduzindo JS JQuery

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+//______________aviso PseudoSite______________
+$(document).ready(function(){    
+    alert("Este site é um protótipo de um e-commerce fictício. A LogoMarca e o nome VipStyle a ele atribuido não possui nenhuma relação com quaisquer marcas registradas e foi usado apenas como inspiração do desenvolvedor Lucas Chagas Ribeiro. O pseudosite pode conter imagens com direitos autorais, o código no entando é livre de royalties e se enquadra na categoria open source (código aberto).");
+    });
+//____________________________________________
