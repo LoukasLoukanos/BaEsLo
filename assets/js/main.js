862b1d0235e8aa6d3a852e17c791cded4898d358
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-12-21 21:12:04 -0300

    Melhorando a arquitetura CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,28 @@
-//______________aviso PseudoSite______________
-$(document).ready(function(){    
+//_________↓JQuery para Registros em localStorage↓_________
+$(document).ready(function(){
+
+    /*insight para área de apresentação: 
+      adiciona a classe "b" no lugar da classe "a" para o #id "for-a"
+        ↓html
+		    <div class="a" id="for-a">
+		        <p>Deveria ser azul, mas fica vermelho</p>
+		    </div>
+	    ↑html    
+        ↓css
+            .b {
+	            color: red;
+            }
+            .a {
+	             color: blue;
+            }
+        ↑css
+    $("#for-a").removeClass("a").addClass("b");
+    */
+
+    //aviso PseudoSite_________
     alert("Este site é um protótipo de um e-commerce fictício. A LogoMarca e o nome VipStyle a ele atribuido não possui nenhuma relação com quaisquer marcas registradas e foi usado apenas como inspiração do desenvolvedor Lucas Chagas Ribeiro. O pseudosite pode conter imagens com direitos autorais, o código no entando é livre de royalties e se enquadra na categoria open source (código aberto).");
-    });
-//____________________________________________
+
+    //localStorage:
+    //...
+});
+//_________↑JQuery para Registros em localStorage↑_________
