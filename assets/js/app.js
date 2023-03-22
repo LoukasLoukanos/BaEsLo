aacc9344a3caa2bc58ce09a028af88f28b518451
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-03-22 17:09:38 -0300

    form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
-import { valida } from './validacao.js'
+// Importa a função 'valida' do módulo './validacao.js'
+import { valida } from './validacao.js';
 
-const inputs = document.querySelectorAll('input')
+// Busca todos os elementos 'input' da página e os armazena em uma constante chamada 'inputs'
+const inputs = document.querySelectorAll('input');
 
-inputs.forEach(input => {
-    input.addEventListener('blur', (evento) => {
-        valida(evento.target)
-    })
-})
-
+// Adiciona um event listener 'blur' a cada elemento 'input' da página
+inputs.input.addEventLiforEach(input => {
+    stener('blur', (evento) => {
+        // Quando ocorre o evento 'blur', chama a função 'valida' e passa o elemento 'input' como argumento
+        valida(evento.target);
+    });
+});
--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -1,31 +1,51 @@
+// Exporta a função 'valida'
 export function valida(input) {
-    const tipoDeInput = input.dataset.tipo
+    // Obtém o tipo de input a partir de um atributo 'data-tipo' do elemento HTML
+    const tipoDeInput = input.dataset.tipo;
 
-    if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
+    // Verifica se existe um validador para o tipo de input e chama o validador correspondente, passando o input como argumento
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
     }
 }
 
+// Cria um objeto com validadores para diferentes tipos de input
 const validadores = {
-    dataServico:input => validaData(input)
-}
+    dataServico: input => validaData(input)
+};
 
+// Função de validação específica para data
 function validaData(input) {
-    const dataRecebida = new Date(input.value)
-    let mensagem = ''
+    // Converte o valor do input em um objeto Date e inicializa a variável de mensagem com uma string vazia
+    const dataRecebida = new Date(input.value);
+    let mensagem = '';
 
-    if(!selecionaData(dataRecebida)) {
-        mensagem = 'Insira a data corretamente.'
+    // Verifica se a data selecionada é válida e define a mensagem de erro se não for
+    if (!selecionaData(dataRecebida)) {
+        mensagem = 'Insira a data corretamente.';
     }
 
-    input.setCustomValidity(mensagem)
+    // Define a mensagem de erro personalizada no elemento HTML usando o método setCustomValidity
+    input.setCustomValidity(mensagem);
 }
 
+// Função auxiliar para verificar se uma data está dentro do intervalo permitido
 function selecionaData(data) {
-    const dataAtual = new Date()
-    const dataSelecionada = new Date(data.getUTCFullYear(), data.getUTCMonth(), data.getUTCDate())
-    const dataMaxima = new Date(data.getUTCFullYear(), data.getUTCMonth() + 3, data.getUTCDate())
+    // Obtém a data atual e converte a data recebida para compará-las
+    const dataAtual = new Date();
+    const dataSelecionada = new Date(
+        data.getUTCFullYear(),
+        data.getUTCMonth(),
+        data.getUTCDate()
+    );
 
-    return (dataSelecionada >= dataAtual) && (dataSelecionada <= dataMaxima)
+    // Define a data máxima permitida como três meses a partir da data atual
+    const dataMaxima = new Date(
+        data.getUTCFullYear(),
+        data.getUTCMonth() + 3,
+        data.getUTCDate()
+    );
+
+    // Retorna true se a data selecionada estiver dentro do intervalo permitido, caso contrário retorna false
+    return (dataSelecionada >= dataAtual) && (dataSelecionada <= dataMaxima);
 }
-
