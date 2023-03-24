631ec8f67dc22336d54b64d34c9a904f8f36826c
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-03-23 21:33:27 -0300

    form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,20 @@
-// Importa a função 'valida' do módulo './validacao.js'
-import { valida } from './validacao.js';
+import { valida } from './validacao.js'
 
-// Busca todos os elementos 'input' da página e os armazena em uma constante chamada 'inputs'
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input')
 
-// Adiciona um event listener 'blur' a cada elemento 'input' da página
-inputs.input.addEventLiforEach(input => {
-    stener('blur', (evento) => {
-        // Quando ocorre o evento 'blur', chama a função 'valida' e passa o elemento 'input' como argumento
-        valida(evento.target);
-    });
-});
+inputs.forEach(input => {
+    if(input.dataset.tipo === 'preco') {
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'R$ ',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+        })
+    }
+
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+    })
+})
--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -1,51 +1,200 @@
-// Exporta a função 'valida'
 export function valida(input) {
-    // Obtém o tipo de input a partir de um atributo 'data-tipo' do elemento HTML
-    const tipoDeInput = input.dataset.tipo;
+    const tipoDeInput = input.dataset.tipo
 
-    // Verifica se existe um validador para o tipo de input e chama o validador correspondente, passando o input como argumento
-    if (validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input);
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
-// Cria um objeto com validadores para diferentes tipos de input
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é válido.' 
+    },
+    cep: {
+        valueMissing: 'O campo de CEP não pode estar vazio.',
+        patternMismatch: 'O CEP digitado não é válido.',
+        customError: 'Não foi possível buscar o CEP.'
+    },
+    logradouro: {
+        valueMissing: 'O campo de logradouro não pode estar vazio.'
+    },
+    cidade: {
+        valueMissing: 'O campo de cidade não pode estar vazio.'
+    },
+    estado: {
+        valueMissing: 'O campo de estado não pode estar vazio.'
+    },
+    preco: {
+        valueMissing: 'O campo de preço não pode estar vazio.'
+    }
+}
+
 const validadores = {
-    dataServico: input => validaData(input)
-};
-
-// Função de validação específica para data
-function validaData(input) {
-    // Converte o valor do input em um objeto Date e inicializa a variável de mensagem com uma string vazia
-    const dataRecebida = new Date(input.value);
-    let mensagem = '';
-
-    // Verifica se a data selecionada é válida e define a mensagem de erro se não for
-    if (!selecionaData(dataRecebida)) {
-        mensagem = 'Insira a data corretamente.';
-    }
-
-    // Define a mensagem de erro personalizada no elemento HTML usando o método setCustomValidity
-    input.setCustomValidity(mensagem);
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input)
 }
 
-// Função auxiliar para verificar se uma data está dentro do intervalo permitido
-function selecionaData(data) {
-    // Obtém a data atual e converte a data recebida para compará-las
-    const dataAtual = new Date();
-    const dataSelecionada = new Date(
-        data.getUTCFullYear(),
-        data.getUTCMonth(),
-        data.getUTCDate()
-    );
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagem
+}
 
-    // Define a data máxima permitida como três meses a partir da data atual
-    const dataMaxima = new Date(
-        data.getUTCFullYear(),
-        data.getUTCMonth() + 3,
-        data.getUTCDate()
-    );
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
 
-    // Retorna true se a data selecionada estiver dentro do intervalo permitido, caso contrário retorna false
-    return (dataSelecionada >= dataAtual) && (dataSelecionada <= dataMaxima);
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
+
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf) {
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
+}
+
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+    if(multiplicador >= 12) {
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for(let contador = 0; multiplicadorInicial > 1 ; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11)
+}
+
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+
+    if(!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data => {
+                if(data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP.')
+                    return
+                }
+                input.setCustomValidity('')
+                preencheCamposComCEP(data)
+                return
+            }
+        )
+    }
+}
+
+function preencheCamposComCEP(data) {
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
+
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
 }
